c27b102027e8711edbf46348697aa16f96d17ba7
NithinNarsepalli <nithinnarsepalli@Nithins-MacBook-Air.local>
2025-07-17 11:14:30 +0530

    Initial project structure with Spring Boot and dependencies

---
+++ src/main/java/com/courseapp/courseSearchApp/CourseSearchAppApplication.java
@@ -0,0 +1,13 @@
+package com.courseapp.courseSearchApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSearchAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSearchAppApplication.class, args);
+	}
+
+}
